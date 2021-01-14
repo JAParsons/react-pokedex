f87f3f59a96b7352d13e0183824332a240676f04
JAParsons <johnparsons932@gmail.com>
2021-01-14 13:39:23 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { yo } from 'packages/pokeapi';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +21,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
