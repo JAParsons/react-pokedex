63502c9e8a95b8ff14c78d21a8c3dffb85cf11a2
JAParsons <johnparsons932@gmail.com>
2021-01-16 18:36:33 +0000

    :construction Using pokeAPI in search-widget

--- src/components/search-widget/search-widget.js
+++ src/components/search-widget/search-widget.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getPokemon } from 'packages/pokeapi';
 
 const SearchWidget = () => {
+  const [searchStr, setSearchStr] = useState('');
+  const [pokemon, setPokemon] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = await getPokemon(searchStr);
+    setPokemon(data);
+  };
+
   return (
     <>
       <p>Search Pokedex</p>
-      <form>
-        <input type="text" placeholder="Enter name or id" />
-        <button>Search</button>
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <input
+          type="text"
+          placeholder="Enter name or id"
+          value={searchStr}
+          onChange={(event) => setSearchStr(event.target.value)}
+        />
+        <button type="submit">Search</button>
       </form>
+      {pokemon ? <p>{JSON.stringify(pokemon)}</p> : null}
     </>
   );
 };
