55f6f22631b41f68e9328b8d5675a237b0ccf46c
JAParsons <johnparsons932@gmail.com>
2021-01-30 19:02:00 +0000

    :white_check_mark: Mocked out axios request

--- src/app.spec.js
+++ src/app.spec.js
@@ -2,8 +2,9 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './app';
+import axios from 'axios';
+jest.mock('axios');
 
-// TODO - mock the fetch call
 describe('App', () => {
   beforeEach(() => {
     // Arrange
@@ -16,12 +17,24 @@ describe('App', () => {
   });
 
   it('can search for a Pokemon', async () => {
+    // Arrange
+    axios.get.mockImplementation(() =>
+      Promise.resolve({
+        data: {
+          name: 'MadeUpPokemon',
+          sprites: {
+            other: { 'official-artwork': { front_default: 'someImageUrl' } }
+          }
+        }
+      })
+    );
+
     // Act
     userEvent.type(screen.getByPlaceholderText('Enter name or id'), 'mew');
     userEvent.click(screen.getByRole('button'));
 
     // Assert
-    expect(await screen.findByText('Mew')).toBeInTheDocument();
+    expect(await screen.findByText('MadeUpPokemon')).toBeInTheDocument();
     expect(await screen.findByRole('img')).toBeInTheDocument();
   });
 });
